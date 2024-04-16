77eee882e296853e61c931514fd4cd5991e620d4
Riwan <coefficriwan@gmail.com>
2024-04-16 18:36:24 +0200

    feat: on granted callback tests, handle_requesting
    - on granted callback can now be a closure
    - handle_requesting implementation, fix incorrect call in release
    - add utilities and tests with on granted callback
    - add test from the report's sequence fairness diagram

--- src/rw_queue.rs
+++ src/rw_queue.rs
@@ -1,4 +1,8 @@
-use std::collections::HashSet;
+use std::{
+    collections::{HashSet, VecDeque},
+    fmt,
+    ops::Deref,
+};
 use thiserror::Error;
 
 #[non_exhaustive]
@@ -13,80 +17,133 @@ pub enum RwQueueError {
 type ProcId = usize;
 
 #[derive(Debug, PartialEq, Eq)]
-enum RequestType {
+pub enum RequestType {
     Writer,
     Reader,
 }
 
-type OnAccessGranted = fn(ProcId);
+pub type OnAccessGranted = Box<dyn Fn(ProcId, RequestType)>;
 
-#[derive(Debug)]
 pub struct RwQueue {
+    on_access_granted: OnAccessGranted,
+    pending_request: VecDeque<(ProcId, RequestType)>,
     readers: HashSet<ProcId>,
     writer: Option<ProcId>,
-    pending_request: Vec<(ProcId, RequestType)>,
-    on_access_granted: OnAccessGranted,
+}
+
+impl fmt::Debug for RwQueue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("RwQueue")
+            .field("on_access_granted", &"OnAccessGranted")
+            .field("pending_request", &self.pending_request)
+            .field("readers", &self.readers)
+            .field("writer", &self.writer)
+            .finish()
+    }
 }
 
 impl RwQueue {
+    /// Handle requesting queue for processus we could not grant access
+    fn handle_requesting(&mut self) {
+        let Some((proc_id, req_type)) = self.pending_request.front() else {
+            return;
+        };
+        match req_type {
+            RequestType::Writer => {
+                self.on_access_granted.deref()(*proc_id, RequestType::Writer);
+                self.register_writer(*proc_id);
+                self.pending_request.pop_front();
+            }
+            RequestType::Reader => {
+                while let Some((proc_id, RequestType::Reader)) =
+                    self.pending_request.front()
+                {
+                    self.on_access_granted.deref()(
+                        *proc_id,
+                        RequestType::Reader,
+                    );
+                    self.register_reader(*proc_id);
+                    self.pending_request.pop_front();
+                }
+            }
+        }
+    }
+
     pub fn new(on_grant: OnAccessGranted) -> RwQueue {
         RwQueue {
             readers: HashSet::new(),
             writer: None,
-            pending_request: Vec::new(),
+            pending_request: VecDeque::new(),
             on_access_granted: on_grant,
         }
     }
 
-    /// Handle requesting queue for processus we could not grant access
-    fn handle_requesting(&mut self) {}
-
     /// Release write / read access by a proc.
-    pub fn release(&mut self, proc: ProcId) -> Result<(), RwQueueError> {
-        let writer_release = self.writer.is_some_and(|writer| writer == proc);
-        let reader_release = self.readers.contains(&proc);
+    pub fn release(&mut self, proc_id: ProcId) -> Result<(), RwQueueError> {
+        let writer_release =
+            self.writer.is_some_and(|writer| writer == proc_id);
+        let reader_release = self.readers.contains(&proc_id);
 
         if !writer_release && !reader_release {
-            return Err(RwQueueError::ReleaseAccess(proc));
+            return Err(RwQueueError::ReleaseAccess(proc_id));
         }
 
         if writer_release {
             self.writer = None;
+            self.handle_requesting();
         } else if reader_release {
-            self.readers.remove(&proc);
+            self.readers.remove(&proc_id);
+            if self.readers.is_empty() {
+                self.handle_requesting();
+            }
         }
 
-        self.handle_requesting();
         Ok(())
     }
 
     /// Request read access by a proc.
     /// Multiple read are possible at the same time.
-    pub fn request_read(&mut self, proc: ProcId) -> Result<(), RwQueueError> {
+    pub fn request_read(
+        &mut self,
+        proc_id: ProcId,
+    ) -> Result<(), RwQueueError> {
         let requesting_writer = self
             .pending_request
-            .first()
+            .front()
             .is_some_and(|req| matches!(req.1, RequestType::Writer));
 
         if self.writer.is_some() || requesting_writer {
-            self.pending_request.push((proc, RequestType::Reader));
-            return Err(RwQueueError::RequestAccess(proc));
+            self.pending_request
+                .push_back((proc_id, RequestType::Reader));
+            return Err(RwQueueError::RequestAccess(proc_id));
         }
 
-        self.readers.insert(proc);
+        self.register_reader(proc_id);
         Ok(())
     }
 
     /// Reqest write access by a proc.
     /// Only one write access at a time.
-    pub fn request_write(&mut self, proc: ProcId) -> Result<(), RwQueueError> {
+    pub fn request_write(
+        &mut self,
+        proc_id: ProcId,
+    ) -> Result<(), RwQueueError> {
         if !self.readers.is_empty() || self.writer.is_some() {
-            self.pending_request.push((proc, RequestType::Writer));
-            return Err(RwQueueError::RequestAccess(proc));
+            self.pending_request
+                .push_back((proc_id, RequestType::Writer));
+            return Err(RwQueueError::RequestAccess(proc_id));
         }
 
-        self.writer = Some(proc);
+        self.register_writer(proc_id);
         Ok(())
+    }
+
+    fn register_reader(&mut self, proc_id: ProcId) {
+        self.readers.insert(proc_id);
+    }
+
+    fn register_writer(&mut self, proc_id: ProcId) {
+        self.writer = Some(proc_id);
     }
 }
 
@@ -94,9 +151,24 @@ impl RwQueue {
 mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
+    use std::sync::mpsc::{channel, Receiver, Sender};
 
     fn create_queue() -> RwQueue {
-        RwQueue::new(|_| {})
+        RwQueue::new(Box::new(|_, _| {}))
+    }
+
+    fn create_queue_with_grant(
+    ) -> (RwQueue, Sender<OnAccessGranted>, Receiver<bool>) {
+        let (fn_tx, fn_rx) = channel::<OnAccessGranted>();
+        let (call_tx, call_rx) = channel();
+        (
+            RwQueue::new(Box::new(move |proc_id, req_type| {
+                fn_rx.try_recv().unwrap()(proc_id, req_type);
+                call_tx.send(true).unwrap();
+            })),
+            fn_tx,
+            call_rx,
+        )
     }
 
     #[test]
@@ -166,6 +238,95 @@ mod tests {
         assert!(queue.release(2).is_err());
         queue.release(1)?;
         assert_eq!(queue.writer, None);
+        Ok(())
+    }
+
+    macro_rules! assert_on_grant {
+        ($tx:expr, $proc:expr, $req:expr) => {
+            $tx.send(Box::new(move |proc_id, req_type| {
+                assert_eq!((proc_id, req_type), ($proc, $req));
+            }))
+            .unwrap();
+        };
+    }
+
+    #[test]
+    fn test_handling_read_before_write() -> Result<(), RwQueueError> {
+        let (mut queue, fn_tx, call_rx) = create_queue_with_grant();
+        queue.request_read(1)?;
+        assert!(queue.request_write(2).is_err());
+        assert!(queue.request_read(3).is_err());
+
+        assert_on_grant!(fn_tx, 2, RequestType::Writer);
+        queue.release(1)?;
+        assert_eq!(call_rx.try_iter().count(), 1);
+
+        assert_on_grant!(fn_tx, 3, RequestType::Reader);
+        queue.release(2)?;
+        assert_eq!(call_rx.try_iter().count(), 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_handling_write_before_read() -> Result<(), RwQueueError> {
+        let (mut queue, fn_tx, call_rx) = create_queue_with_grant();
+        queue.request_write(1)?;
+        assert!(queue.request_read(2).is_err());
+        assert!(queue.request_read(3).is_err());
+        assert!(queue.request_read(4).is_err());
+        assert!(queue.request_write(5).is_err());
+
+        for i in 2..=4 {
+            assert_on_grant!(fn_tx, i, RequestType::Reader);
+        }
+        queue.release(1)?;
+        assert_eq!(call_rx.try_iter().count(), 3);
+
+        assert_on_grant!(fn_tx, 5, RequestType::Writer);
+        queue.release(2)?;
+        queue.release(3)?;
+        assert_eq!(call_rx.try_iter().count(), 0);
+        queue.release(4)?;
+        assert_eq!(call_rx.try_iter().count(), 1);
+
+        Ok(())
+    }
+
+    // correspond to the report fairness diagram
+    #[test]
+    fn test_fairness() -> Result<(), RwQueueError> {
+        let (mut queue, fn_tx, call_rx) = create_queue_with_grant();
+        let (a, b, c, d) = (1, 2, 3, 4);
+
+        queue.request_read(a)?;
+        assert!(queue.request_write(c).is_err());
+        assert!(queue.request_read(b).is_err());
+        assert_eq!(
+            queue.pending_request,
+            vec![(c, RequestType::Writer), (b, RequestType::Reader)]
+        );
+
+        assert_on_grant!(fn_tx, c, RequestType::Writer);
+        queue.release(a)?;
+        assert_eq!(call_rx.try_iter().count(), 1);
+        assert_eq!(queue.pending_request, vec![(b, RequestType::Reader)]);
+
+        assert!(queue.request_read(d).is_err());
+        assert_eq!(
+            queue.pending_request,
+            vec![(b, RequestType::Reader), (d, RequestType::Reader)]
+        );
+
+        assert_on_grant!(fn_tx, b, RequestType::Reader);
+        assert_on_grant!(fn_tx, d, RequestType::Reader);
+        queue.release(c)?;
+        assert_eq!(call_rx.try_iter().count(), 2);
+
+        assert!(queue.pending_request.is_empty());
+        assert_eq!(queue.readers, HashSet::from([b, d]));
+        assert!(queue.writer.is_none());
+
         Ok(())
     }
 }
