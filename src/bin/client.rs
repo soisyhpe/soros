6734cad31ed8fe0eca66a93a87c01a29095d258b
Eros Chan (soisyhpe) <soisyhpe@gmail.com>
2024-05-21 09:37:00 +0200

    feat(mirror server): now ask for secondary server information

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,14 +1,19 @@
-use std::{env, thread};
+use std::net::{IpAddr, SocketAddr};
 use std::sync::Arc;
+use std::thread::sleep;
+use std::time::Duration;
+use std::{env, thread};
 
 use env_logger::Env;
 use log::{error, info, warn};
-use thiserror::Error;
+use soros::p2p_server::{
+    DataStore, DataStoreError, P2PServer, P2PServerError,
+};
 use soros::{
     handle_wait_error,
     protocol_client::{ProtocolClient, ProtocolClientError},
 };
-use soros::peer2peer_server::{DataStore, P2PServer, P2PServerError};
+use thiserror::Error;
 
 #[non_exhaustive]
 #[derive(Debug, Error)]
@@ -17,40 +22,94 @@ pub enum ClientError {
     ProtocolClientError(#[from] ProtocolClientError),
     #[error("P2PServer error: {0}")]
     P2PServerError(#[from] P2PServerError),
+    #[error("DataStore error: {0}")]
+    DataStoreError(#[from] DataStoreError),
 }
 
 fn create_protocol_client() -> Result<ProtocolClient, ProtocolClientError> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        panic!("Usage: {} <hostname> <server port>", args[0]);
+    if args.len() != 5 {
+        panic!("Usage: {} <primary hostname> <primary port> <secondary hostname> <secondary port>", args[0]);
     }
 
-    let hostname: String = args[1].parse().expect("Invalid hostname");
-    let port: u16 = args[2].parse().expect("Invalid port number");
+    let primary_hostname: String = args[1].parse().expect("Invalid primary hostname");
+    let primary_port: u16 = args[2].parse().expect("Invalid primary port number");
+    let secondary_hostname: String = args[3].parse().expect("Invalid secondary hostname");
+    let secondary_port: u16 = args[4].parse().expect("Invalid secondary port number");
 
-    ProtocolClient::new(&hostname, port)
+    ProtocolClient::new(&primary_hostname, primary_port, &secondary_hostname, secondary_port)
 }
 
-fn create_p2p_server() -> Result<P2PServer, P2PServerError> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        panic!("Usage: {} <hostname> <server port>", args[0]);
-    }
+fn p2p() -> Result<(), ClientError> {
+    // client1
+    let mut client1 = create_protocol_client()?;
+    let client1_datastore = Arc::new(DataStore::new());
+    let data_store1 = client1_datastore.clone();
 
-    let hostname: String = args[1].parse().expect("Invalid hostname");
-    let port: u16 = args[2].parse().expect("Invalid port number");
+    let handle1 = thread::spawn(|| -> Result<(), P2PServerError> {
+        let mut client1_p2p = P2PServer::new(data_store1, "127.0.0.1", 6001)?;
+        client1_p2p.bind()?;
+        Ok(())
+    });
 
-    let datastore = Arc::new(DataStore::new());
+    sleep(Duration::from_secs(1));
 
-    P2PServer::new(datastore, &hostname, port + 12) // + 12 because "peer to peer" is 12 characters long
+    let data_key = 1;
+    client1_datastore.clone().create(data_key, "my_data")?;
+    info!(
+        "Data with key {} created with content {}",
+        data_key,
+        client1_datastore.get(&data_key)?
+    );
+
+    client1.registry_create(data_key)?;
+    let _ = client1.registry_create(data_key);
+    client1.registry_write_sync(data_key)?;
+    info!("Write of the data with key {}", data_key);
+
+    client1_datastore
+        .clone()
+        .write(&data_key, "modified_data")?;
+    info!(
+        "Data with key {} modified with content {}",
+        data_key,
+        client1_datastore.get(&data_key)?
+    );
+
+    client1.registry_release(data_key)?;
+
+    // client2
+    let mut _client2 = create_protocol_client()?;
+    // let client2_datastore = Arc::new(DataStore::new());
+    // let data_store2 = client2_datastore.clone();
+
+    // let handle2 = thread::spawn(|| -> Result<(), P2PServerError> {
+    //     let mut client2_p2p = P2PServer::new(data_store2, "localhost", 6001)?;
+    //     client2_p2p.bind()?;
+    //     Ok(())
+    // });
+
+    // TODO: let holder = client1.registry_read_sync(data_key)?;
+    let holder =
+        SocketAddr::new(IpAddr::V4("127.0.0.1".parse().unwrap()), 6001);
+    info!("Read of the data with key {}, holder: {}", data_key, holder);
+
+    P2PServer::get(holder.ip().to_string().as_str(), holder.port(), data_key)?;
+    // info!("Read data {}", data);
+
+    client1.registry_delete(data_key)?;
+
+    let _ = handle1.join().unwrap();
+    // let _ = handle2.join();
+
+    Ok(())
 }
 
 fn basic_usage() -> Result<(), ClientError> {
     let mut protocol_client = create_protocol_client()?;
-    let mut _p2p_server = create_p2p_server()?;
 
-    let mut _data = "my_data".to_string();
     let data_key = 1;
+    let mut _data = "my_data".to_string();
 
     protocol_client.registry_create(data_key)?;
     let _ = protocol_client.registry_create(data_key);
@@ -74,7 +133,6 @@ fn basic_usage() -> Result<(), ClientError> {
 
 fn advanced_usage() -> Result<(), ClientError> {
     let mut protocol_client = create_protocol_client()?;
-    let mut _p2p_server = create_p2p_server()?;
 
     let data_key = 2;
 
@@ -103,8 +161,9 @@ fn main() -> Result<(), ClientError> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info"))
         .init();
 
-    let handles =
-        vec![thread::spawn(basic_usage), thread::spawn(advanced_usage)];
+    let handles = vec![
+        /*thread::spawn(p2p)*/ thread::spawn(basic_usage), /*thread::spawn(advanced_usage)*/
+    ];
 
     for handle in handles {
         let _ = handle
